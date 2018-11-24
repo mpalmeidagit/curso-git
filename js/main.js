e6444a3aa1b4a7ea991fc628aa1fcc3e128d5203
Marivaldo Almeida <marivaldoalmeida@yahoo.com.br>
2018-11-24 20:24:23 -0200

    Adicionado a pasta js e o arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();        // Sorts the elements of fruits
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();        // Sorts the elements of fruits
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();        // Sorts the elements of fruits
